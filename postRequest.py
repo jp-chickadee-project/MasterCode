f27f89c0c0c7c6fc803e2fb330ecfd135f1bccc5
Ryan <frizfraz@rvr3561librarywifi237.nmu.edu>
2018-06-19 12:07:54 -0400

    Testing for other computer

--- postRequest.py
+++ postRequest.py
@@ -7,12 +7,12 @@ import RPi.GPIO as GPIO
 
 
 SERVER_API = "http://euclid.nmu.edu:18155/api/visits/"
-DATA = #Figure out how to auto-fill 'data' section of post() request.
+DATA =         #Figure out how to auto-fill 'data' section of post() request. maybe use a method to compile it and once compiled put back into
 
 def main():
     GPIO.setup(23, GPIO.OUT)
     GPIO.output(23, False)
-    PortRF = serial.Serial('/dev/serial0', 9600)
+    port_RFID = serial.Serial('/dev/serial0', 9600)
 
     readCounter = 0
     ID = ""
@@ -20,17 +20,17 @@ def main():
     prevTime = 0
 
     while True:
-        read_byte = PortRF.read()
+        read_byte = port_RFID.read()
         if read_byte == b'\x02':
             for bCounter in range(10):
-                read_byte = PortRF.read()
+                read_byte = port_RFID.read()
                 ID = ID + read_byte.decode("utf-8")
 
             timeStamp = int(time.time())
             if ID == prevID and timeStamp - prevTime < 2:
                 GPIO.output(23, False)
                 time.sleep(.500)
-                PortRF.flushInput()
+                port_RFID.flushInput()
                 ID = ""
                 continue
             PrevID = ID
