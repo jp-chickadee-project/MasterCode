5b93cff62bb1299754e202ed81c9c2e7016913d8
rangerTech24 <38325282+rangerTech24@users.noreply.github.com>
2019-10-08 20:14:52 -0400

    moved login files to node directory

--- login_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#
-echo "alias transmit='sudo lmic-rpi-lora-gps-hat/examples/transmit/build/transmit.out'" >> .bashrc
-echo "alias brain='sudo python3 ~/MasterCode/brain.py'" >> .bashrc
-echo ". MasterCode/login.sh" >> .bashrc
-
-echo "this script will now delete itself"
-rm -- $0
